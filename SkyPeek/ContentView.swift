1afe6c778b7d018eedad95cfb2f8206174becdbb
ShreyasTantri <shreyastantrik29@gmail.com>
2026-05-22 22:38:52 +0530

    Initial Commit

---
+++ SkyPeek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyPeek
+//
+//  Created by CCS038 on 22/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyPeek/SkyPeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyPeekApp.swift
+//  SkyPeek
+//
+//  Created by CCS038 on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyPeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
